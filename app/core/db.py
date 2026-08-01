e22d3154d988085aad2e1b4f89b89da48e79213e
James Peters <jamespeters92@gmail.com>
2026-08-01 08:26:01 +0100

    feat: CHOR-15: Amend pg session retrieval

--- app/core/db.py
+++ app/core/db.py
@@ -1,5 +1,10 @@
+from typing import Annotated
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Session
+
+from fastapi import Depends
 
 from app.core.config import settings
 
@@ -7,12 +12,10 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SessionDep = Annotated[Session, Depends(get_session)]
